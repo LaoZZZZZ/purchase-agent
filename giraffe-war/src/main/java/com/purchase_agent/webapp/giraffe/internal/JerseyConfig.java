84acdc48c6cee5464f2642f63be8e94c3c3715cd
HAHA <luzhao1986@gmail.com>
2017-02-19 00:42:23 -0800

    register all added entities.

--- giraffe-war/src/main/java/com/purchase_agent/webapp/giraffe/internal/JerseyConfig.java
+++ giraffe-war/src/main/java/com/purchase_agent/webapp/giraffe/internal/JerseyConfig.java
@@ -1,16 +1,27 @@
 package com.purchase_agent.webapp.giraffe.internal;
 
+
+import com.googlecode.objectify.ObjectifyService;
 import com.purchase_agent.webapp.giraffe.greetings.Greetings;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
+import com.purchase_agent.webapp.giraffe.objectify_entity.Entities;
 
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         // Enable Spring DI
         register(RequestContextFilter.class);
-	register(JacksonObjectMapperConfig.class);
+        registerEntities();
+	    register(JacksonObjectMapperConfig.class);
 	
-	// Application resource
-	register(Greetings.class);
+	    // Application resource
+	    register(Greetings.class);
+
+    }
+
+    private void registerEntities() {
+        for (final Object ob : Entities.entities) {
+            ObjectifyService.register((java.lang.Class)ob);
+        }
     }
 }
