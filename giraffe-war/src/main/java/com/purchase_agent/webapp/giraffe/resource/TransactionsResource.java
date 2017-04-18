80942fc5b0d7167e52159e05c252f326e461e347
HAHA <luzhao1986@gmail.com>
2017-04-17 23:07:34 -0700

    change unnecessary modification.

--- giraffe-war/src/main/java/com/purchase_agent/webapp/giraffe/resource/TransactionsResource.java
+++ giraffe-war/src/main/java/com/purchase_agent/webapp/giraffe/resource/TransactionsResource.java
@@ -18,7 +18,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -31,7 +30,7 @@ import static com.googlecode.objectify.ObjectifyService.ofy;
 /**
  * Created by lukez on 3/9/17.
  */
-@Path("/lineItems")
+@Path("/transactions")
 @Produces(MediaType.APPLICATION_JSON)
 public class TransactionsResource {
     private static final Logger logger = Logger.getLogger(TransactionsResource.class.getName());
